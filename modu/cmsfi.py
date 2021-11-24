dbe2ad0ba408524805f30950fcee9c941d779f65
lemonlove7 <56328995+lemonlove7@users.noreply.github.com>
2021-11-25 00:29:10 +0800

    Update cmsfi.py

--- modu/cmsfi.py
+++ modu/cmsfi.py
@@ -1,9 +1,8 @@
 import requests
 import zlib
 import json
-from requests.packages.urllib3.exceptions import InsecureRequestWarning  #消除警告
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)  # 消除警告
-
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 def whatweb(url):
     response = requests.get(url,verify=False)
     whatweb_dict = {"url":response.url,"text":response.text,"headers":dict(response.headers)}
@@ -12,10 +11,3 @@ def whatweb(url):
     whatweb_dict = zlib.compress(whatweb_dict)
     data = {"info":whatweb_dict}
     return requests.post("http://whatweb.bugscaner.com/api.go",files=data)
-
-# if __name__ == '__main__':
-#     url=input('请输入要检查的url：')
-#     request = whatweb(url=url)
-#     print(u"今日识别剩余次数")
-#     print(request.headers["X-RateLimit-Remaining"])
-#     print(f'cms识别结果：{request.json()}')
